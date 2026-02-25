975a82cfc7b4320e28a18acf113a1a7fd84b4c2a
dekana576 <dekana576@gmail.com>
2026-02-25 14:47:09 +0800

    make axios page

---
+++ latihan-hero-ui/app/axios/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import {Card, CardBody} from "@heroui/react";
+
+export default function AxiosPage() {
+  return (
+    <h1 className="text-4xl font-bold mb-8">Axios Example</h1>
+  )
+}   
