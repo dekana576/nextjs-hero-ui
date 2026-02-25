d043c4ee7cbb5beba1137542e959a87673b5ac86
dekana576 <dekana576@gmail.com>
2026-02-25 10:21:02 +0800

    Tes Hero UI

---
+++ latihan-hero-ui/app/components/HeroUiButton.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@heroui/react";
+
+const HeroUiButton = () => {
+  return (
+      <Button color="primary" variant="solid">
+        Test HeroUI Button
+      </Button>
+  );
+}
+
+export default HeroUiButton;
