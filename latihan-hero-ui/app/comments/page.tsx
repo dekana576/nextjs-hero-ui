6d078aef5f92468362c006fe36d104a6742f2fd9
dekana576 <dekana576@gmail.com>
2026-02-27 15:28:00 +0800

    add pagination to comments page

--- latihan-hero-ui/app/comments/page.tsx
+++ latihan-hero-ui/app/comments/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import CardList from "../components/CardList";
 import axios from "axios";
-
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Pagination } from "@heroui/react";
+import { useState } from "react";
 
 const base_url = "https://jsonplaceholder.typicode.com/comments";
 
@@ -16,18 +17,30 @@ interface Icomment {
 }
 
 export default function Comments() {
-  const [comments, setComments] = useState<Icomment[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9;
 
-  useEffect(() => {
-    axios
-      .get<Icomment[]>(base_url)
-      .then((response) => {
-        setComments(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  const queryClient = useQueryClient();
+  const {
+    data: comments,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["comments"],
+    queryFn: async () => {
+      const response = await axios.get<Icomment[]>(base_url);
+      return response.data;
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) throw error;
+
+  const totalPages = Math.ceil((comments?.length || 0) / itemsPerPage);
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const currentComments = comments?.slice(start, end);
   return (
     <div>
       <CardList>
@@ -35,23 +48,33 @@ export default function Comments() {
       </CardList>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {comments.map((comment) => (
-        <CardList key={comment.id}>
-          <p>
-            <strong>ID:</strong> {comment.id}
-          </p>
-          <h2>
-            <strong>Name:</strong> {comment.name}
-          </h2>
-          <h2>
-            <strong>Email:</strong> {comment.email}
-          </h2>
-          <p>
-            <strong>Body:</strong> {comment.body}
-          </p>
-        </CardList>
-      ))}
+        {currentComments?.map((comment) => (
+          <CardList key={comment.id}>
+            <p>
+              <strong>ID:</strong> {comment.id}
+            </p>
+            <h2>
+              <strong>Name:</strong> {comment.name}
+            </h2>
+            <h2>
+              <strong>Email:</strong> {comment.email}
+            </h2>
+            <p>
+              <strong>Body:</strong> {comment.body}
+            </p>
+          </CardList>
+        ))}
       </div>
+        <div className="flex justify-center my-6">
+          <Pagination
+            total={totalPages}
+            page={currentPage}
+            onChange={setCurrentPage}
+            showControls
+            isCompact
+            color="warning"
+          />
+        </div>
     </div>
   );
 }
